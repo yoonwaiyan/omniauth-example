be2dad6d5d76f0ab4973dc155f2b13562cbad2ad
Yoon Wai Yan <waiyan@cloudcoder.com.my>
2015-11-23 15:13:56 +0800

    add facebook provider and rough homepage

--- Gemfile
+++ Gemfile
@@ -22,10 +22,12 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
-gem 'devise'
 gem 'bootstrap-sass', '~> 3.3.5'
+
+# Devise and Omniauth
+gem 'devise'
 gem 'devise-bootstrap-views'
+gem 'omniauth-facebook'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -39,6 +41,7 @@ gem 'devise-bootstrap-views'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry-rails'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+
+  class << self
+		def from_omniauth(auth)
+			puts "~~~~auth"
+			p auth
+			# binding.pry
+			identity = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |identity|
+			  identity.provider     = auth.provider
+			  identity.uid          = auth.uid
+			  identity.email        = auth.info.email if auth.info.email
+			  identity.password     = Devise.friendly_token.first(8)
+			end
+			identity.save!
+			identity
+		end
+	end
+
 end
